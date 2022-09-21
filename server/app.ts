2c4ce4d7bcdec78bc73ebedd7f4589edb538307d
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2022-09-20 23:52:41 -0700

    Server React Router support

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import config from './config.json'
+import path from 'path'
 
 const PORT = config.port || 3000
 const server = express()
 
 server.use(express.static('../client/build'))
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.resolve('../client/build/index.html'))
+})
 
 server.listen(PORT)
 console.log(`Listening on port ${PORT}`)
