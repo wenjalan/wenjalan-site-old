b259e61b34fafce9e0666a1a76c22a6e10e621ea
Alan Wen <36051502+wenjalan@users.noreply.github.com>
2022-09-20 18:39:11 -0700

    Projects page

--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,5 +1,21 @@
+import { Container, Stack, List } from "@mui/material"
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+import { ResumeListItem } from "../components/ResumeListItem"
+import projectsJson from "../content/projects.json"
+
 function Projects() {
-  return <h1>Projects</h1>
+  return (
+    <Container>
+      <Stack>
+        <Navbar />
+        <Header value="Projects" />
+        <List>
+          {projectsJson.map(item => <ResumeListItem item={item} />)}
+        </List>
+      </Stack>
+    </Container>
+  )
 }
 
 export default Projects
